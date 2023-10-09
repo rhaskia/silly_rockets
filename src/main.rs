dd860eeb13589ead85d06eb315c3422617f4a93b
Rhaskia <94507929+rhaskia@users.noreply.github.com>
2023-10-09 20:29:05 +0000

    switch to bevy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use macroquad::prelude::*;
+use bevy::prelude::*;
 
-#[macroquad::main("Texture")]
-async fn main() {
-    let texture: Texture2D = load_texture("sprites/grass.png").await.unwrap();
-
-    loop {
-        clear_background(LIGHTGRAY);
-        draw_texture(&texture, screen_width() / 2., screen_height() / 2., WHITE);
-        next_frame().await
-    }
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
 }
