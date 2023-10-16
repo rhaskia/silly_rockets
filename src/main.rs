03fcb11d420b7575ddab2cd9b545a70392a4b20b
rhaskia <94507929+RhaskiaGameDev@users.noreply.github.com>
2023-10-16 20:25:24 +1300

    hrhrhrh

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,39 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-#[derive(Component)]
-struct PlayerData {
-    name: String
-}
-
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_systems(Startup, setup)
-        .add_systems(Update, print_ball_altitude)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(setup_graphics)
+        .add_startup_system(setup_physics)
+        .add_system(print_ball_altitude)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
     commands.spawn(Camera2dBundle::default());
+}
 
-    commands.spawn(RigidBody::Dynamic)
-        .insert(SpriteBundle {
-            texture: asset_server.load("sprites/player.png"),
-            ..default()
-        })
-        .insert(Velocity {
-            linvel: Vec2::new(1.0, 2.0),
-            angvel: 0.2
-        })
-        .insert(GravityScale(0.5))
-        .insert(Sleeping::disabled())
-        .insert(Ccd::enabled());
+fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(SpriteBundle { texture: asset_server.load("sprites/player.png"), ..default() })
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
 }
 
 fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
     for transform in positions.iter() {
         println!("Ball altitude: {}", transform.translation.y);
-    }
-}
-
-fn player_movement(
-    mut player_q: Query<(&mut Transform, &mut PlayerData)>,
-    keys: Res<Input<KeyCode>>,
-    time: Res<Time>,
-) {
-    for (mut trans, mut data) in &mut player_q {
-        if keys.pressed(KeyCode::W) {
-        trans.translation.x += 30.* time.delta_seconds();
-        }
     }
 }
